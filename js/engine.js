6b55dcd7c912df3de76829b0d7e93b080374b1fb
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2018-04-23 20:29:32 +0200

    creating a page with the name of the studio

--- js/engine.js
+++ js/engine.js
@@ -1,8 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () { 
+document.addEventListener("DOMContentLoaded", function () {
 console.log("DOM fully loaded and parsed!");
 
+//created intro
+$('body').html($('<div>', {class: 'mainContainer'}));
 
-
-
-
+let mainContainerDiv = $(".mainContainer");
+let unfaithfulDogsDiv = $("<div class='unfaithfulDogs'>");
+mainContainerDiv.append(unfaithfulDogsDiv);
+$(".unfaithfulDogs").text("NIEWIERNE PSY");
 });
