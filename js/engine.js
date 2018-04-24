ae349bd87248a1c6c180fcbb84e97259810cc5a0
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2018-04-24 20:18:43 +0200

    change font, first animate added

--- js/engine.js
+++ js/engine.js
@@ -1,11 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
 console.log("DOM fully loaded and parsed!");
 
-//created intro
+//main container added to body
 $('body').html($('<div>', {class: 'mainContainer'}));
 
+//prefabrication of main elements
 let mainContainerDiv = $(".mainContainer");
-let unfaithfulDogsDiv = $("<div class='unfaithfulDogs'>");
-mainContainerDiv.append(unfaithfulDogsDiv);
-$(".unfaithfulDogs").text("NIEWIERNE PSY");
+let newDiv = $("<div>");
+
+//INTRO
+//inscription NIEWIERNE Psy added to main container
+mainContainerDiv.append(newDiv);
+newDiv.addClass("unfaithfulDogs").text("NIEWIERNE PSY");
+
+// animated removing inscription NIEWIRNE PSY
+hideInscription = () => {
+  $(".unfaithfulDogs").animate({opacity: "0"}, 3000);
+}
+setTimeout(hideInscription, 4000);
+
 });
