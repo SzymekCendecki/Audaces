1704c5e8bbe17bc9c22641ad50759e673e57a0a9
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2018-04-25 19:11:54 +0200

    new concept, new intro

--- js/engine.js
+++ js/engine.js
@@ -1,22 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
 console.log("DOM fully loaded and parsed!");
 
-//main container added to body
-$('body').html($('<div>', {class: 'mainContainer'}));
+// hidden elements
+$("#title").hide();
 
-//prefabrication of main elements
-let mainContainerDiv = $(".mainContainer");
-let newDiv = $("<div>");
-
-//INTRO
-//inscription NIEWIERNE Psy added to main container
-mainContainerDiv.append(newDiv);
-newDiv.addClass("unfaithfulDogs").text("NIEWIERNE PSY");
-
-// animated removing inscription NIEWIRNE PSY
-hideInscription = () => {
-  $(".unfaithfulDogs").animate({opacity: "0"}, 3000);
+//intro
+//inscription
+let hideInscription = () => {
+  $("#inscriptions").fadeOut(2000);
 }
-setTimeout(hideInscription, 4000);
+setTimeout(hideInscription, 5000);
 
+//title
+let showTitle = () => {
+  $("#title").fadeIn(2000);
+}
+setTimeout(showTitle, 7500);
+
+let hideTitle = () => {
+  $("#title").fadeOut(2000);
+}
+setTimeout(hideTitle, 12000);
 });
