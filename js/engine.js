2be495036b1722fdd50c9ef859f14ec46004f28d
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2018-04-29 20:16:12 +0200

    delete Ajax and write few more lines

--- js/engine.js
+++ js/engine.js
@@ -1,9 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
 console.log("DOM fully loaded and parsed!");
-
-// hidden elements
-$("#title, #startOptionsGame, #hello").hide();
-
 //intro
 //inscription
 let hideInscription = () => {
@@ -22,6 +18,9 @@ let hideTitle = () => {
 }
 setTimeout(hideTitle, 12000);
 
+// hidden elements
+$("#title, #startOptionsGame, #hello, #chooseBtns").hide();
+
 //Start of game - option choice
 let startOptionsGame = () => {
   $("#startOptionsGame, #hello").fadeIn(2000);
@@ -29,45 +28,38 @@ let startOptionsGame = () => {
 }
 setTimeout(startOptionsGame, 14500);
 
-var url = "http://localhost:3000";
-
 //info button
-$(".btnInfo").on("click", function(){
-  $.ajax({
-    url: url + "/menu",
-    type: "json",
-    method: "GET"
-  }).done(function(response){
-    $("#descriptionMenu").text(response[0].btnInfo).addClass("descriptionMenu");
-  }).fail(function(error){
-    console.log("Something going wrong with Your code");
-   });
+$(".btnInfo").on("click", () => {
+  $("#descriptionMenu").text("Ta wersja gry została stworzona jako projekt końcowy kursu front'endowego, a także jako modernizacja tej samej gry sprzed kursu. Gra ma także na celu przetestowanie mechaniki 'papierowej' wersji systemu gry Audaces. Projekt będzie rozwijalny także po skończeniu kursu. Wszystkie wersje tej gry zostały wydane przez niezależne studio Niewierne Psy. Gra powstała dzięki użytym technologiom: HTML5, SASS(css), jQuery, EcmaScript 6. Fonty pobrane z: fonts.google.com/.").addClass("descriptionMenu");
 });
 
 //licence button
-$(".btnLicence").on("click", function(){
-  $.ajax({
-    url: url + "/menu",
-    type: "json",
-    method: "GET"
-  }).done(function(response){
-    $("#descriptionMenu").text(response[0].btnLicence).addClass("descriptionMenu");
-  }).fail(function(error){
-    console.log("Something going wrong with Your code");
-   });
+$(".btnLicence").on("click", () =>{
+  $("#descriptionMenu").text("UMOWA LICENCYJNA UŻYTKOWNIKA APLIKACJI AUDACES (oraz wszystkich jego wesji). POSIADAJĄC APLIKACJĘ AKCEPTUJESZ PONIŻSZE WARUNKI LICENCJI. Autor informuje, iż zawarte treści w aplikacji mogą być niestosowne dla osób niepełnoletnich, o innym światopoglądzie lub wyznawanej religii. Użytkownik używa aplikację na własne ryzyko. Twórca tej aplikacji infourmuje, iż aplikacja ta została stowrzona w celach hobyystycznych oraz w celu nauki programowania. Aplikacja ta może zawierać błędy, które niekoniecznie muszą być poprawione. Autor nie udziela żadnych gwarancji, obietnic oraz zapewnień na tą aplikację (i jej wersje) i nie odpowiada za błędne działanie lub użytkowanie programu. Autor aplikacji nie bierze na siebie żadnych reklamacji w sprawie wadliwego działania programu oraz nie poczuwa się do zadość uczynienia za spowodowane szkody. Prawa autorskie należą do Szymona Cendeckiego. Audace jest rozprowadzany na zasadzie FREEWARE czyli: użytkownik dostaje aplikację za darmo, nie mozę w żaden sposób zmieniać aplikacji, może wykorzystywać aplikację na własny użytek, zabrnia się wykorzystywać aplikację w celach komercyjnych, zabrania się ujawniania kodu źródłowego.").addClass("descriptionMenu");
 });
 
 //tutorial button
-$(".btnTutorial").on("click", function(){
-  $.ajax({
-    url: url + "/menu",
-    type: "json",
-    method: "GET"
-  }).done(function(response){
-    $("#descriptionMenu").text(response[0].btnTutorial).addClass("descriptionMenu");
-  }).fail(function(error){
-    console.log("Something going wrong with Your code");
-   });
+$(".btnTutorial").on("click", () =>{
+    $("#descriptionMenu").text("Początek gry. Ekran składa się z powitania oraz czterech przycisków. Po wciśnięciu przycisku Info, Licencja lub Tutorial pojawi się pod nimi odpowiednia informacj. Po wciśnięciu przycisku Nowa gra, użytkownik rozpocznie grę. Po rozpoczęciu gry, użytkownik będzie mógł wylosować postać lub ją stworzyć. Gdy będzie chciał ją stworzyć samemu, przejdzie do kreatora, który będzie podpowiadał możliwości. Po stworzeniu postaci, użytkownik przejdze do gry. Ekran będzie podzielony zasadniczo na dwie części. Górna, miejsza, będzie zawierała przyciski, dzięki którym będzie można mieć podgląd do charakterystyk swojego protagonisty, ekwipunku zadań do wykonania oraz pomocy. Dolna część to część, na której będzie się odgrywała cała historia gry. Na niej to gracz będzie mógł dokonywać interakcji.").addClass("descriptionMenu");
+});
+
+//new game button
+$(".btnNewGame").on("click", () => {
+  $("#hello, #startOptionsGame, #descriptionMenu").fadeOut(750);
+
+  let showChooseBtns = () => {
+      $("#chooseBtns").fadeIn(750);
+  }
+  setTimeout(showChooseBtns, 1000);
+});
+
+//random Hero button and create Hero button
+$("#randomHero").on("mouseenter", () => {
+  $("#chooseBtnsDescription").text("Losowanie postaci oraz automatyczne rozpoczęcie gry.").addClass("randomHeroDescription");
+});
+
+$("#randomHero").on("mouseleave", () => {
+  $("#chooseBtnsDescription").text("").removeClass("randomHeroDescription");
 });
 
 });
---
+++ js/intro.js
@@ -0,0 +1,17 @@
+//intro
+//inscription
+let hideInscription = () => {
+  $("#inscriptions").fadeOut(2000);
+}
+setTimeout(hideInscription, 5000);
+
+//title
+let showTitle = () => {
+  $("#title").fadeIn(2000);
+}
+setTimeout(showTitle, 7500);
+
+let hideTitle = () => {
+  $("#title").fadeOut(2000);
+}
+setTimeout(hideTitle, 12000);
