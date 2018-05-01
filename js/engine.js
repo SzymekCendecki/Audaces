893f750b00ab9d32ec1eb835c18de303431c2d82
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2018-05-01 12:59:25 +0200

    new structure file, creator begin

--- js/engine.js
+++ js/engine.js
@@ -67,9 +67,7 @@
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(1);
-__webpack_require__(2);
-module.exports = __webpack_require__(3);
+module.exports = __webpack_require__(1);
 
 
 /***/ }),
@@ -80,91 +78,118 @@ module.exports = __webpack_require__(3);
 
 
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("NIEWIERNE PSY RULEZ!!!!");
+    console.log("NIEWIERNE PSY RULEZ!!!!");
 
-  //intro
-  //inscription
-  var hideInscription = function hideInscription() {
-    $("#inscriptions").fadeOut(2000);
-  };
-  setTimeout(hideInscription, 5000);
+    // hidden elements
+    $("#title, #startOptionsGame, #hello, .slider, #slider").hide();
 
-  //title
-  var showTitle = function showTitle() {
-    $("#title").fadeIn(2000);
-  };
-  setTimeout(showTitle, 7500);
-
-  var hideTitle = function hideTitle() {
-    $("#title").fadeOut(2000);
-  };
-  setTimeout(hideTitle, 12000);
-
-  // hidden elements
-  $("#title, #startOptionsGame, #hello, #chooseBtns").hide();
-});
-
-/***/ }),
-/* 2 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("NIEWIERNE PSY RULEZ!!!!");
-
-  //Start of game - option choice
-  var startOptionsGame = function startOptionsGame() {
-    $("#startOptionsGame, #hello").fadeIn(2000);
-    $("body").css("backgroundColor", "beige");
-  };
-  setTimeout(startOptionsGame, 14500);
-
-  //info button
-  $(".btnInfo").on("click", function () {
-    $("#descriptionMenu").text("Ta wersja gry została stworzona jako projekt końcowy kursu front'endowego, a także jako modernizacja tej samej gry sprzed kursu. Gra ma także na celu przetestowanie mechaniki 'papierowej' wersji systemu gry Audaces. Projekt będzie rozwijalny także po skończeniu kursu. Wszystkie wersje tej gry zostały wydane przez niezależne studio Niewierne Psy. Gra powstała dzięki użytym technologiom: HTML5, SASS(css), jQuery, EcmaScript 6. Fonty pobrane z: fonts.google.com/.").addClass("descriptionMenu");
-  });
-
-  //licence button
-  $(".btnLicence").on("click", function () {
-    $("#descriptionMenu").text("UMOWA LICENCYJNA UŻYTKOWNIKA APLIKACJI AUDACES (oraz wszystkich jego wesji). POSIADAJĄC APLIKACJĘ AKCEPTUJESZ PONIŻSZE WARUNKI LICENCJI. Autor informuje, iż zawarte treści w aplikacji mogą być niestosowne dla osób niepełnoletnich, o innym światopoglądzie lub wyznawanej religii. Użytkownik używa aplikację na własne ryzyko. Twórca tej aplikacji infourmuje, iż aplikacja ta została stowrzona w celach hobyystycznych oraz w celu nauki programowania. Aplikacja ta może zawierać błędy, które niekoniecznie muszą być poprawione. Autor nie udziela żadnych gwarancji, obietnic oraz zapewnień na tą aplikację (i jej wersje) i nie odpowiada za błędne działanie lub użytkowanie programu. Autor aplikacji nie bierze na siebie żadnych reklamacji w sprawie wadliwego działania programu oraz nie poczuwa się do zadość uczynienia za spowodowane szkody. Prawa autorskie należą do Szymona Cendeckiego. Audace jest rozprowadzany na zasadzie FREEWARE czyli: użytkownik dostaje aplikację za darmo, nie mozę w żaden sposób zmieniać aplikacji, może wykorzystywać aplikację na własny użytek, zabrnia się wykorzystywać aplikację w celach komercyjnych, zabrania się ujawniania kodu źródłowego.").addClass("descriptionMenu");
-  });
-
-  //tutorial button
-  $(".btnTutorial").on("click", function () {
-    $("#descriptionMenu").text("Początek gry. Ekran składa się z powitania oraz czterech przycisków. Po wciśnięciu przycisku Info, Licencja lub Tutorial pojawi się pod nimi odpowiednia informacj. Po wciśnięciu przycisku Nowa gra, użytkownik rozpocznie grę. Po rozpoczęciu gry, użytkownik będzie mógł wylosować postać lub ją stworzyć. Gdy będzie chciał ją stworzyć samemu, przejdzie do kreatora, który będzie podpowiadał możliwości. Po stworzeniu postaci, użytkownik przejdze do gry. Ekran będzie podzielony zasadniczo na dwie części. Górna, miejsza, będzie zawierała przyciski, dzięki którym będzie można mieć podgląd do charakterystyk swojego protagonisty, ekwipunku zadań do wykonania oraz pomocy. Dolna część to część, na której będzie się odgrywała cała historia gry. Na niej to gracz będzie mógł dokonywać interakcji.").addClass("descriptionMenu");
-  });
-
-  //new game button
-  $(".btnNewGame").on("click", function () {
-    $("#hello, #startOptionsGame, #descriptionMenu").fadeOut(750);
-
-    var showChooseBtns = function showChooseBtns() {
-      $("#chooseBtns").fadeIn(750);
+    //intro
+    //inscription
+    var hideInscription = function hideInscription() {
+        $("#inscriptions").fadeOut(2000);
     };
-    setTimeout(showChooseBtns, 1000);
-  });
-});
+    setTimeout(hideInscription, 5000);
 
-/***/ }),
-/* 3 */
-/***/ (function(module, exports, __webpack_require__) {
+    //title
+    var showTitle = function showTitle() {
+        $("#title").fadeIn(2000);
+    };
+    setTimeout(showTitle, 7500);
 
-"use strict";
+    var hideTitle = function hideTitle() {
+        $("#title").fadeOut(2000);
+    };
+    setTimeout(hideTitle, 12000);
 
+    //Start of game - option choice
+    var startOptionsGame = function startOptionsGame() {
+        $("#startOptionsGame, #hello").fadeIn(2000);
+        $("body").css("backgroundColor", "beige");
+    };
+    setTimeout(startOptionsGame, 14500);
 
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("NIEWIERNE PSY RULEZ!!!!");
+    //info button
+    $(".btnInfo").on("click", function () {
+        $("#descriptionMenu").text("Ta wersja gry została stworzona jako projekt końcowy kursu front'endowego, a także jako modernizacja tej samej gry sprzed kursu. Gra ma także na celu przetestowanie mechaniki 'papierowej' wersji systemu gry Audaces. Projekt będzie rozwijalny także po skończeniu kursu. Wszystkie wersje tej gry zostały wydane przez niezależne studio Niewierne Psy. Gra powstała dzięki użytym technologiom: HTML5, SASS(css), jQuery, EcmaScript 6. Fonty pobrane z: fonts.google.com/.").addClass("descriptionMenu");
+    });
 
-  //random Hero button and create Hero button
-  $("#randomHero").on("mouseenter", function () {
-    $("#chooseBtnsDescription").text("Losowanie postaci oraz automatyczne rozpoczęcie gry.").addClass("randomHeroDescription");
-  });
+    //licence button
+    $(".btnLicence").on("click", function () {
+        $("#descriptionMenu").text("UMOWA LICENCYJNA UŻYTKOWNIKA APLIKACJI AUDACES (oraz wszystkich jego wesji). POSIADAJĄC APLIKACJĘ AKCEPTUJESZ PONIŻSZE WARUNKI LICENCJI. Autor informuje, iż zawarte treści w aplikacji mogą być niestosowne dla osób niepełnoletnich, o innym światopoglądzie lub wyznawanej religii. Użytkownik używa aplikację na własne ryzyko. Twórca tej aplikacji infourmuje, iż aplikacja ta została stowrzona w celach hobyystycznych oraz w celu nauki programowania. Aplikacja ta może zawierać błędy, które niekoniecznie muszą być poprawione. Autor nie udziela żadnych gwarancji, obietnic oraz zapewnień na tą aplikację (i jej wersje) i nie odpowiada za błędne działanie lub użytkowanie programu. Autor aplikacji nie bierze na siebie żadnych reklamacji w sprawie wadliwego działania programu oraz nie poczuwa się do zadość uczynienia za spowodowane szkody. Prawa autorskie należą do Szymona Cendeckiego. Audace jest rozprowadzany na zasadzie FREEWARE czyli: użytkownik dostaje aplikację za darmo, nie mozę w żaden sposób zmieniać aplikacji, może wykorzystywać aplikację na własny użytek, zabrnia się wykorzystywać aplikację w celach komercyjnych, zabrania się ujawniania kodu źródłowego.").addClass("descriptionMenu");
+    });
 
-  $("#randomHero").on("mouseleave", function () {
-    $("#chooseBtnsDescription").text("").removeClass("randomHeroDescription");
-  });
+    //tutorial button
+    $(".btnTutorial").on("click", function () {
+        $("#descriptionMenu").text("Początek gry. Ekran składa się z powitania oraz czterech przycisków. Po wciśnięciu przycisku Info, Licencja lub Tutorial pojawi się pod nimi odpowiednia informacj. Po wciśnięciu przycisku Nowa gra, użytkownik rozpocznie grę. Po rozpoczęciu gry, użytkownik będzie mógł wylosować postać lub ją stworzyć. Gdy będzie chciał ją stworzyć samemu, przejdzie do kreatora, który będzie podpowiadał możliwości. Po stworzeniu postaci, użytkownik przejdze do gry. Ekran będzie podzielony zasadniczo na dwie części. Górna, miejsza, będzie zawierała przyciski, dzięki którym będzie można mieć podgląd do charakterystyk swojego protagonisty, ekwipunku zadań do wykonania oraz pomocy. Dolna część to część, na której będzie się odgrywała cała historia gry. Na niej to gracz będzie mógł dokonywać interakcji.").addClass("descriptionMenu");
+    });
+
+    //Creator of hero
+    //Arrays for creator
+    //main races
+    var races = ["człowiek", "elf", "krasnolud", "ork"];
+
+    //main professions
+    var professions = ["wojownik", "czarodziej", "złoczyńca"];
+
+    //basic items - start of game
+    //weapons = 15
+    var weaponArray = ["sztylet", "daga", "mizerykordia", "krótki miecz", "szabla", "miecz długi", "miecz półtoraręczny", "miecz dwuręczny", "espadon", "włócznia", "pika", "halabarda", "proca", "łuk", "kusza"];
+
+    //armors = 7
+    var armorsArray = ["przeszywanica", "zbroja skórzana", "zbroja ćwiekowana", "zbroja paskowa", "kolczuga", "zbroja pópłytowa", "zbroja płytowa"];
+
+    //shields = 6
+    var shieldsArray = ["puklerz", "mała drewniana", "mała metalowa", "duża drewniana", "duża metalowa", "pawęż"];
+
+    //equipment and skills
+    //equipment = 19
+    var equipment = ["hubka i krzesiwo", "mieszek", "pas skórzany", "igły i nici", "tuba na pergaminy", "pęk piór do pisania", "pergamin 1szt.", "zwykłe ubranie", "płaszcz", "fikuśny kapelusz", "łapcie z łyka", "kosz wiklinowy", "plecak", "torba podróżna", "sakwa", "koc", "namiot", "drewniana miska", "drewniana łyżka"];
+
+    //skills 10
+    var skills = ["survival", "dyscyplina", "dowodzenie", "uderzenie tarczą", "jeździectwo", //worrior
+    "powożenie zaprzęgiem", "przygotowanie trucizn", "wspinaczka", "aktorstwo", "akrobatyka", //bandit
+    "pisanie i czytanie", "przywołanie/odpędzenie demona", "wróżbiarstwo", "leczenie ran", "rzucanie czarów" //wizard
+    ];
+
+    //slider
+    $(".btnNewGame").on("click", function () {
+        $("#hello, #startOptionsGame, #descriptionMenu").fadeOut(750);
+        $("#slider").delay(750).fadeIn(750);
+        var slideCount = $('#slider ul li').length;
+        var slideWidth = $('#slider ul li').width();
+        var slideHeight = $('#slider ul li').height();
+        var sliderUlWidth = slideCount * slideWidth;
+
+        $('#slider').css({ width: slideWidth, height: slideHeight });
+        $('#slider ul').css({ width: sliderUlWidth, marginLeft: -slideWidth });
+        $('#slider ul li:last-child').prependTo('#slider ul');
+
+        function moveLeft() {
+            $('#slider ul').animate({
+                left: +slideWidth
+            }, 200, function () {
+                $('#slider ul li:last-child').prependTo('#slider ul');
+                $('#slider ul').css('left', '');
+            });
+        };
+
+        function moveRight() {
+            $('#slider ul').animate({
+                left: -slideWidth
+            }, 200, function () {
+                $('#slider ul li:first-child').appendTo('#slider ul');
+                $('#slider ul').css('left', '');
+            });
+        };
+
+        $('button.control_prev').click(function () {
+            moveLeft();
+        });
+
+        $('button.control_next').click(function () {
+            moveRight();
+        });
+    });
 });
 
 /***/ })
---
+++ js/workingFile.js
@@ -0,0 +1,115 @@
+document.addEventListener("DOMContentLoaded", () => {
+console.log("NIEWIERNE PSY RULEZ!!!!");
+
+// hidden elements
+$("#title, #startOptionsGame, #hello, .slider, #slider").hide();
+
+//intro
+//inscription
+let hideInscription = () => {
+  $("#inscriptions").fadeOut(2000);
+}
+setTimeout(hideInscription, 5000);
+
+//title
+let showTitle = () => {
+  $("#title").fadeIn(2000);
+}
+setTimeout(showTitle, 7500);
+
+let hideTitle = () => {
+  $("#title").fadeOut(2000);
+}
+setTimeout(hideTitle, 12000);
+
+//Start of game - option choice
+let startOptionsGame = () => {
+  $("#startOptionsGame, #hello").fadeIn(2000);
+  $("body").css("backgroundColor", "beige");
+}
+setTimeout(startOptionsGame, 14500);
+
+//info button
+$(".btnInfo").on("click", () => {
+  $("#descriptionMenu").text("Ta wersja gry została stworzona jako projekt końcowy kursu front'endowego, a także jako modernizacja tej samej gry sprzed kursu. Gra ma także na celu przetestowanie mechaniki 'papierowej' wersji systemu gry Audaces. Projekt będzie rozwijalny także po skończeniu kursu. Wszystkie wersje tej gry zostały wydane przez niezależne studio Niewierne Psy. Gra powstała dzięki użytym technologiom: HTML5, SASS(css), jQuery, EcmaScript 6. Fonty pobrane z: fonts.google.com/.").addClass("descriptionMenu");
+});
+
+//licence button
+$(".btnLicence").on("click", () =>{
+  $("#descriptionMenu").text("UMOWA LICENCYJNA UŻYTKOWNIKA APLIKACJI AUDACES (oraz wszystkich jego wesji). POSIADAJĄC APLIKACJĘ AKCEPTUJESZ PONIŻSZE WARUNKI LICENCJI. Autor informuje, iż zawarte treści w aplikacji mogą być niestosowne dla osób niepełnoletnich, o innym światopoglądzie lub wyznawanej religii. Użytkownik używa aplikację na własne ryzyko. Twórca tej aplikacji infourmuje, iż aplikacja ta została stowrzona w celach hobyystycznych oraz w celu nauki programowania. Aplikacja ta może zawierać błędy, które niekoniecznie muszą być poprawione. Autor nie udziela żadnych gwarancji, obietnic oraz zapewnień na tą aplikację (i jej wersje) i nie odpowiada za błędne działanie lub użytkowanie programu. Autor aplikacji nie bierze na siebie żadnych reklamacji w sprawie wadliwego działania programu oraz nie poczuwa się do zadość uczynienia za spowodowane szkody. Prawa autorskie należą do Szymona Cendeckiego. Audace jest rozprowadzany na zasadzie FREEWARE czyli: użytkownik dostaje aplikację za darmo, nie mozę w żaden sposób zmieniać aplikacji, może wykorzystywać aplikację na własny użytek, zabrnia się wykorzystywać aplikację w celach komercyjnych, zabrania się ujawniania kodu źródłowego.").addClass("descriptionMenu");
+});
+
+//tutorial button
+$(".btnTutorial").on("click", () =>{
+    $("#descriptionMenu").text("Początek gry. Ekran składa się z powitania oraz czterech przycisków. Po wciśnięciu przycisku Info, Licencja lub Tutorial pojawi się pod nimi odpowiednia informacj. Po wciśnięciu przycisku Nowa gra, użytkownik rozpocznie grę. Po rozpoczęciu gry, użytkownik będzie mógł wylosować postać lub ją stworzyć. Gdy będzie chciał ją stworzyć samemu, przejdzie do kreatora, który będzie podpowiadał możliwości. Po stworzeniu postaci, użytkownik przejdze do gry. Ekran będzie podzielony zasadniczo na dwie części. Górna, miejsza, będzie zawierała przyciski, dzięki którym będzie można mieć podgląd do charakterystyk swojego protagonisty, ekwipunku zadań do wykonania oraz pomocy. Dolna część to część, na której będzie się odgrywała cała historia gry. Na niej to gracz będzie mógł dokonywać interakcji.").addClass("descriptionMenu");
+});
+
+//Creator of hero
+//Arrays for creator
+//main races
+let races = ["człowiek", "elf", "krasnolud", "ork"];
+
+//main professions
+let professions = ["wojownik", "czarodziej", "złoczyńca"];
+
+//basic items - start of game
+//weapons = 15
+let weaponArray = ["sztylet", "daga", "mizerykordia", "krótki miecz", "szabla", "miecz długi", "miecz półtoraręczny", "miecz dwuręczny", "espadon", "włócznia", "pika", "halabarda", "proca", "łuk", "kusza"];
+
+//armors = 7
+let armorsArray = ["przeszywanica", "zbroja skórzana", "zbroja ćwiekowana", "zbroja paskowa", "kolczuga", "zbroja pópłytowa", "zbroja płytowa"];
+
+//shields = 6
+let shieldsArray = ["puklerz", "mała drewniana", "mała metalowa", "duża drewniana", "duża metalowa", "pawęż"];
+
+//equipment and skills
+//equipment = 19
+let equipment = ["hubka i krzesiwo", "mieszek", "pas skórzany", "igły i nici", "tuba na pergaminy", "pęk piór do pisania", "pergamin 1szt.", "zwykłe ubranie", "płaszcz", "fikuśny kapelusz", "łapcie z łyka", "kosz wiklinowy", "plecak", "torba podróżna", "sakwa", "koc", "namiot", "drewniana miska", "drewniana łyżka"];
+
+//skills 10
+let skills = ["survival", "dyscyplina", "dowodzenie", "uderzenie tarczą", "jeździectwo", //worrior
+"powożenie zaprzęgiem", "przygotowanie trucizn", "wspinaczka", "aktorstwo", "akrobatyka", //bandit
+"pisanie i czytanie", "przywołanie/odpędzenie demona", "wróżbiarstwo", "leczenie ran", "rzucanie czarów" //wizard
+];
+
+//slider
+$(".btnNewGame").on("click", () => {
+  $("#hello, #startOptionsGame, #descriptionMenu").fadeOut(750);
+  $("#slider").delay(750).fadeIn(750);
+  var slideCount = $('#slider ul li').length;
+	var slideWidth = $('#slider ul li').width();
+	var slideHeight = $('#slider ul li').height();
+	var sliderUlWidth = slideCount * slideWidth;
+
+	$('#slider').css({ width: slideWidth, height: slideHeight });
+	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+	$('#slider ul li:last-child').prependTo('#slider ul');
+
+    function moveLeft() {
+        $('#slider ul').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('#slider ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    $('button.control_prev').click(function () {
+        moveLeft();
+    });
+
+    $('button.control_next').click(function () {
+        moveRight();
+    });
+});
+
+});
