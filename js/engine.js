c8c53e3275cf9a1505fa8b211523dc4485759267
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2018-05-08 22:30:08 +0200

    small fix choose skills

--- js/engine.js
+++ js/engine.js
@@ -886,7 +886,12 @@ module.exports.skill = function () {
   var skillArray = [];
 
   $.each($("input[name='skill']:checked"), function () {
-    skillArray.push($(this).val());
+
+    if (skillArray.indexOf($(this).val()) !== -1) {
+      console.log("Masz już tą umiejętność. wybierz inną");
+    } else {
+      skillArray.push($(this).val());
+    }
 
     if (skillArray.length > 3) {
       $('#alertSkill').text("Nie wybrać więcej niż 3 umiejętności.");
--- js/slides/sixthSlide.js
+++ js/slides/sixthSlide.js
@@ -2,7 +2,12 @@ module.exports.skill = function(){
   let skillArray = [];
 
   $.each($("input[name='skill']:checked"), function(){
-              skillArray.push($(this).val());
+
+            if(skillArray.indexOf($(this).val()) !== -1){
+                console.log("Masz już tą umiejętność. wybierz inną");
+              }else{
+                skillArray.push($(this).val());
+              }
 
               if(skillArray.length > 3){
                 $('#alertSkill').text("Nie wybrać więcej niż 3 umiejętności.");
